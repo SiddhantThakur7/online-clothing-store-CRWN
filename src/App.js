12313c4241cb412b82a9426c3a14ec75f63534fb
Sidreg1113 <siddhant.thakur7@gmail.com>
2021-04-25 20:41:42 +0530

    Setup the user info to be stored in the data base. The sign in with google now saves the user info to firestore and returns. Setup a function to return a snapshot of the document who is signing up if it exists. Synced status of the current user to the snapshot being returned, logging out resets the current user to none.

--- src/App.js
+++ src/App.js
@@ -7,23 +7,39 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component.jsx";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 class App extends React.Component {
-
-  constructor(){
+  constructor() {
     super();
 
     this.state = {
-      currentUser: null
+      currentUser: null,
     };
   }
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        console.log(userAuth);
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
-    })
+        userRef.onSnapshot(
+          (snapShot) => {
+            this.setState({
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data(),
+              }
+            },
+            () => {
+              console.log(this.state);
+            }
+            );
+          });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+    });
   }
 
   componentWillUnmount() {
@@ -33,7 +49,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser}/>
+        <Header currentUser={this.state.currentUser} />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
@@ -42,7 +58,6 @@ class App extends React.Component {
       </div>
     );
   }
-  
 }
 
 export default App;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,31 @@ var firebaseConfig = {
   appId: "1:866653539626:web:171ca6fba2c6b228e1a253",
   measurementId: "G-DZGBZ4P6N2",
 };
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) {
+    return;
+  }
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email, 
+        createdAt
+      })
+    } catch (error) {
+      console.log('error in creating user.', error.message);
+    }
+  }
+  return userRef;
+};
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -23,6 +48,5 @@ export const firestore = firebase.firestore();
 // provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
-
 
 export default firebase;
