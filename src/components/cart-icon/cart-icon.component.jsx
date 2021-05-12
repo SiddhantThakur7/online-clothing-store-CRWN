f7db249874dd0fa5ace71e22dfc360258d439f47
Sidreg1113 <siddhant.thakur7@gmail.com>
2021-05-12 11:13:58 +0530

    Added the functionalitty of memoization to stop any residual re renderinng. Only the state related to the particular component will be rendered using the selecters created using the reselect library and wont be rendering again the whole function until and unless the values have changed.

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,18 +3,23 @@ import React from "react";
 import { ReactComponent as Shoppingicon } from "../../assets/11.1 shopping-bag.svg";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import { connect } from "react-redux";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ itemCount, toggleCartHidden }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <Shoppingicon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state)
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
