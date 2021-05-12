8ab4f35d4159f419764773c3b8341500ce1b105d
Sidreg1113 <siddhant.thakur7@gmail.com>
2021-05-12 18:44:57 +0530

    Removing Logging Messeges

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 // putting the logger function inside an array, can add several othwer middleware functions too.
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV == 'development') {
+        middlewares.push('logger');
+}
 
 // do not need to add seperate individual middlewares therefore providing abstraction to the number and names of the middle wares to be implemented
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
